d69e76f7efe9cfc579bd1e2088ba4f75b6f8d499
Yazan Qawasmeh <yazan929@users.noreply.github.com>
2021-01-18 11:57:21 +0800

    restart

--- src/ShoeList.js
+++ src/ShoeList.js
@@ -1,45 +1,36 @@
-import React from "react";
-import "./App.css";
-import Modal from "@material-ui/core/Modal";
-import Backdrop from "@material-ui/core/Backdrop";
-import Fade from "@material-ui/core/Fade";
-import { withStyles } from "@material-ui/core/styles";
-import ShoeInfoComponent from "./ShoeInfoComponent";
+import React from 'react';
+import './App.css';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import { withStyles } from '@material-ui/core/styles';
 
 const useStyles = (theme) => ({
     modal: {
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
     paper: {
         backgroundColor: theme.palette.success.light,
-        border: "2px solid #000",
+        border: '2px solid #000',
         boxShadow: theme.shadows[5],
         padding: theme.spacing(2, 4, 3),
     },
 });
 
 class ShoeList extends React.Component {
-    // constructor(props) {
-    //     super(props);
+    constructor(props) {
+        super(props);
 
-    //     this.state = {
-    //     };
+        this.state = {
+            isModalOpen: false,
+            currentShoeIndex: 0
+        };
 
-    // }
+        this.handleClose = this.handleClose.bind(this);
+    }
 
-    state = {
-        visible: false,
-    };
-
-    showModal = () => {
-        this.setState({ visible: true });
-    };
-
-    hideModal = () => {
-        this.setState({ visible: false });
-    };
     shoesArray = [
         {
             photo: "photos/Men1.jpg",
@@ -131,46 +122,90 @@ class ShoeList extends React.Component {
             class: "KShoes",
             name: "shoe18",
         },
-    ];
+
+    ]
+
+    handleClose = () => {
+        this.setState({
+            isModalOpen: false,
+            //currentShoeIndex: -1,
+        });
+    };
 
     render() {
         const { classes } = this.props;
 
         return (
-            <div>
-                {this.state.visible ? (
-                    <div className="Modal">
-                        <div className="InsideModal">
-                            <button aria-label="Close" onClick={this.hideModal}>
-                                X
-                            </button>
-                            <p>I'm A Pop Up!!!</p>
-                            <img src={shoe.photo} />
-                        </div>
-                    </div>
-                ) : null}
-                <div className="Shoes">
+            <div className="Shoes">
                 <div className="innerShoe">
+                    { /* VISIBLE */ }
                     {this.shoesArray.map((shoe, index) => {
-                        return (
-                            <div
-                                key={index.toString()}
-                                className={shoe.class}
-                                onClick={() => {
-                                    this.showModal({
-
-                                    });
-                                }}
-                            >
-                                <img src={shoe.photo} />
-                            </div>
-                        );
+                        return <div key={index.toString()} className={shoe.class}
+                            onClick={() => {
+                                this.setState({
+                                    isModalOpen: true,
+                                    currentShoeIndex: index,
+                                });
+                            }}>
+                            <img src={shoe.photo} />
+                        </div>;
                     })}
+
+                    { /* NOT VISIBLE */ }
+                    <Modal
+                        aria-labelledby="transition-modal-title"
+                        aria-describedby="transition-modal-description"
+                        className={classes.modal}
+                        open={this.state.isModalOpen}
+                        onClose={this.handleClose}
+                        closeAfterTransition
+                        BackdropComponent={Backdrop}
+                        BackdropProps={{
+                            timeout: 500,
+                        }}
+                    >
+                        <Fade in={this.state.isModalOpen}>
+                            <div className={classes.paper}>
+                                <div className="modalSize">
+                                    <div  className="modal-close" onClick={() => {
+                                        this.setState({
+                                            isModalOpen: false,
+                                            
+                                        });
+                                    }}>
+                                        <button  title="Close" >X </button></div>
+
+                                    <h1 className="modalText">This is {this.shoesArray[this.state.currentShoeIndex].name}</h1>
+                                    <p className="modalImg"><img src={this.shoesArray[this.state.currentShoeIndex].photo}></img></p>
+                                    <div className="buttonDiv" >
+                                        <button > Buy now!</button>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </Fade>
+                    </Modal>
                 </div>
-            </div>
             </div>
         );
     }
 }
 
-export default withStyles(useStyles)(ShoeList);
+export default withStyles(useStyles)(ShoeList)
+
+
+
+
+
+
+
+// this.state.currentShoeIndex > -1 ? this.shoesArray[this.state.currentShoeIndex].name : ''; 
+
+// () => {
+// if (this.state.currentShoeIndex > -1) {
+//     return this.shoesArray[this.state.currentShoeIndex].name;
+// }
+// else {
+//     return '';
+// }
+// }
