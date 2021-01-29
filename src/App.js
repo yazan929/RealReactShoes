b67856a87140cd3861b8a82c0523c7f078cccfd8
Yazan Qawasmeh <yazan929@users.noreply.github.com>
2021-01-29 16:07:06 +0800

    redux worked.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,115 @@
-import logo from './logo.svg';
 import Footer from './Footer';
 import Header from './Header';
 import BreakPart from './Break';
 import ShoeList from './ShoeList';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 
+function reducer(){
+  return{
+    shoesArray : [
+      {
+          photo: "photos/Men1.jpg",
+          class: "Mshoes",
+          name: "shoe1",
+      },
+      {
+          photo: "photos/Men2.jpg",
+          class: "Mshoes",
+          name: "shoe2",
+      },
+      {
+          photo: "photos/Men3.jpg",
+          class: "Mshoes",
+          name: "shoe3",
+      },
+      {
+          photo: "photos/Men4.jpg",
+          class: "Mshoes",
+          name: "shoe4",
+      },
+      {
+          photo: "photos/Men5.jpg",
+          class: "Mshoes",
+          name: "shoe5",
+      },
+      {
+          photo: "photos/Men6.jpg",
+          class: "Mshoes",
+          name: "shoe6",
+      },
+      {
+          photo: "photos/women1.jpg",
+          class: "WShoes",
+          name: "shoe7",
+      },
+      {
+          photo: "photos/women2.jpg",
+          class: "WShoes",
+          name: "shoe8",
+      },
+      {
+          photo: "photos/women3.jpg",
+          class: "WShoes",
+          name: "shoe9",
+      },
+      {
+          photo: "photos/women4.jpg",
+          class: "WShoes",
+          name: "shoe10",
+      },
+      {
+          photo: "photos/women5.jpg",
+          class: "WShoes",
+          name: "shoe11",
+      },
+      {
+          photo: "photos/women6.jpg",
+          class: "WShoes",
+          name: "shoe12",
+      },
+      {
+          photo: "photos/Kids1.jpg",
+          class: "KShoes",
+          name: "shoe13",
+      },
+      {
+          photo: "photos/Kids2.jpg",
+          class: "KShoes",
+          name: "shoe14",
+      },
+      {
+          photo: "photos/Kids3.jpg",
+          class: "KShoes",
+          name: "shoe15",
+      },
+      {
+          photo: "photos/Kids4.jpg",
+          class: "KShoes",
+          name: "shoe16",
+      },
+      {
+          photo: "photos/Kids5.jpg",
+          class: "KShoes",
+          name: "shoe17",
+      },
+      {
+          photo: "photos/Kids6.jpg",
+          class: "KShoes",
+          name: "shoe18",
+      },
 
-
+  ]
+  };
+}
+const store = createStore(reducer);
 
 function App() {
 
-  return (    
+  return (  
+    <Provider store={store}>
+
+
       <div className="App">
         <Header />
         
@@ -19,6 +119,7 @@ function App() {
 
         <Footer />
       </div>    
+      </Provider>  
   );
 }
 
--- src/ShoeList.js
+++ src/ShoeList.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
-import ShoeInfoComponent from "./ShoeInfoComponent";
+import {connect} from 'react-redux';
+// console.log(this.props)
 
 class ShoeList extends React.Component {
     constructor(props) {
         super(props);
-        // this.showModal = this.showModal.bind(this);
-        this.hideModal = this.hideModal.bind(this);
+        this.showModal = this.showModal.bind(this);
+        // this.hideModal = this.hideModal.bind(this);
     }
 
     state = {
@@ -23,100 +24,8 @@ class ShoeList extends React.Component {
         this.setState({ visible: false });
     };
 
-    shoesArray = [
-        {
-            photo: "photos/Men1.jpg",
-            class: "Mshoes",
-            name: "shoe1",
-        },
-        {
-            photo: "photos/Men2.jpg",
-            class: "Mshoes",
-            name: "shoe2",
-        },
-        {
-            photo: "photos/Men3.jpg",
-            class: "Mshoes",
-            name: "shoe3",
-        },
-        {
-            photo: "photos/Men4.jpg",
-            class: "Mshoes",
-            name: "shoe4",
-        },
-        {
-            photo: "photos/Men5.jpg",
-            class: "Mshoes",
-            name: "shoe5",
-        },
-        {
-            photo: "photos/Men6.jpg",
-            class: "Mshoes",
-            name: "shoe6",
-        },
-        {
-            photo: "photos/women1.jpg",
-            class: "WShoes",
-            name: "shoe7",
-        },
-        {
-            photo: "photos/women2.jpg",
-            class: "WShoes",
-            name: "shoe8",
-        },
-        {
-            photo: "photos/women3.jpg",
-            class: "WShoes",
-            name: "shoe9",
-        },
-        {
-            photo: "photos/women4.jpg",
-            class: "WShoes",
-            name: "shoe10",
-        },
-        {
-            photo: "photos/women5.jpg",
-            class: "WShoes",
-            name: "shoe11",
-        },
-        {
-            photo: "photos/women6.jpg",
-            class: "WShoes",
-            name: "shoe12",
-        },
-        {
-            photo: "photos/Kids1.jpg",
-            class: "KShoes",
-            name: "shoe13",
-        },
-        {
-            photo: "photos/Kids2.jpg",
-            class: "KShoes",
-            name: "shoe14",
-        },
-        {
-            photo: "photos/Kids3.jpg",
-            class: "KShoes",
-            name: "shoe15",
-        },
-        {
-            photo: "photos/Kids4.jpg",
-            class: "KShoes",
-            name: "shoe16",
-        },
-        {
-            photo: "photos/Kids5.jpg",
-            class: "KShoes",
-            name: "shoe17",
-        },
-        {
-            photo: "photos/Kids6.jpg",
-            class: "KShoes",
-            name: "shoe18",
-        },
-    ];
-
     render() {
+
         return (
             <div>
                 <div>
@@ -130,9 +39,9 @@ class ShoeList extends React.Component {
                                 >
                                     X
                                 </button>
-                                <p>This Is Shoe  {this.shoesArray[this.state.currentShoeIndex].name}</p>
+                                <p>This Is Shoe  {this.props.shoesArray[this.state.currentShoeIndex].name}</p>
 
-                                <img  src={this.shoesArray[this.state.currentShoeIndex].photo}></img>
+                                <img  src={this.props.shoesArray[this.state.currentShoeIndex].photo}></img>
                             </div>
                         </div>
                     ) : null}
@@ -141,11 +50,11 @@ class ShoeList extends React.Component {
                 <div className="Shoes">
                     <div className="innerShoe">
                         {/* VISIBLE */}
-                        {this.shoesArray.map((shoe, index) => {
+                        {this.props.shoesArray.map((shoe, index) => {
                             return (
                                 <div
-                                    key={index.toString()}
-                                    className={shoe.class}
+                                    key={index.toString()} 
+                                    className={shoe} 
                                     onClick={() => {
                                         {this.showModal();
                                             this.setState({ currentShoeIndex: index });
@@ -154,7 +63,7 @@ class ShoeList extends React.Component {
                                         
                                     }}
                                 >
-                                    <img src={shoe.photo} />
+                                    <img src={this.props.shoesArray[index].photo} />
                                 </div>
                             );
                         })}
@@ -165,4 +74,12 @@ class ShoeList extends React.Component {
     }
 }
 
-export default ShoeList;
+const mapStateToProps = (state) =>{
+    return{
+        shoesArray: state.shoesArray,
+        
+    }
+
+}
+
+export default connect(mapStateToProps) (ShoeList);
