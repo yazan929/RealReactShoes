0b381653e410f69da79361593ec7bd797747ede7
Yazan Qawasmeh <yazan929@users.noreply.github.com>
2021-02-15 04:04:26 +0800

    fixed string

--- src/components/shoes.js
+++ src/components/shoes.js
@@ -1,32 +1,32 @@
-import React from "react";
-import { useEffect, useState } from "react";
+// import React from "react";
+// import { useEffect, useState } from "react";
 
-export const Shoes = () => {
-    const [initialState, setInitialState] = useState([])
+// export const Shoes = () => {
+//     const [initialState, setInitialState] = useState([])
 
-    useEffect(() => {
-        fetch('/api/')
-            .then((res) => {
-                if (res.ok) {
-                    return res.json();
-                }
-            })
+//     useEffect(() => {
+//         fetch('/api/')
+//             .then((res) => {
+//                 if (res.ok) {
+//                     return res.json();
+//                 }
+//             })
             
-        .then((jsonResponse) => setInitialState(jsonResponse.shoes))
-      //  // .then((jsonResponse) => console.log(jsonResponse));
-    }, []);
-    console.log(initialState);
+//         .then((jsonResponse) => setInitialState(jsonResponse.shoes))
+//       //  // .then((jsonResponse) => console.log(jsonResponse));
+//     }, []);
+//     console.log(initialState);
 
-    // // return (<div>{insialState.length>0 && insialState.map((e,i) => <li key={i}>{e}</li>)}</div>);
+//     // // return (<div>{insialState.length>0 && insialState.map((e,i) => <li key={i}>{e}</li>)}</div>);
 
-    return (
-        <div>
-            {/* <div>{initialState}</div> */}
-            <div>
-                {" "}
-                {initialState.length > 0 &&
-                    initialState.map((e) => <li>{e}</li>)}
-            </div>
-        </div>
-    );
-};
+//     return (
+//         <div>
+//             {/* <div>{initialState}</div> */}
+//             <div>
+//                 {" "}
+//                 {initialState.length > 0 &&
+//                     initialState.map((e) => <li>{e}</li>)}
+//             </div>
+//         </div>
+//     );
+// };
