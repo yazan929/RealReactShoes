08e14e5b758bde701ab203ff9ec0aab4611c0319
Yazan Qawasmeh <yazan929@users.noreply.github.com>
2021-04-05 12:19:36 +0800

    java test

---
+++ react-server/src/main/java/com/yazan929/reactshoes/ReactServerApplication.java
@@ -0,0 +1,14 @@
+package com.yazan929.reactshoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactServerApplication.class, args);
+		System.out.println("@@@ app Started");
+	}
+
+}
---
+++ react-server/src/test/java/com/yazan929/reactshoes/ReactServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yazan929.reactshoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
