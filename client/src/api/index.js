1fade27993bd4d16a2890a76e4251a2cc495a279
Yazan Qawasmeh <yazan929@users.noreply.github.com>
2021-04-08 19:11:28 +0800

    full stack java backend works

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:8080/api/shoes";
+const url = "http://ec2-54-179-117-71.ap-southeast-1.compute.amazonaws.com:8080/api/shoes";
 
 export const fetchPosts = () => axios.get(url);
 
