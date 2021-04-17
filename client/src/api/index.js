feb2722fbe714227885263c95160a1af50c210ba
Yazan Qawasmeh <yazan929@users.noreply.github.com>
2021-04-18 04:18:49 +0800

    ready for aws

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const url = "http://ec2-54-179-117-71.ap-southeast-1.compute.amazonaws.com:8080/api/shoes";
-const url = "http://localhost:6039/all";
+const url = "http://ec2-54-179-117-71.ap-southeast-1.compute.amazonaws.com:8080/api/shoes";
+// const url = "http://localhost:6039/all";
 export const fetchPosts = () => axios.get(url);
 
 
