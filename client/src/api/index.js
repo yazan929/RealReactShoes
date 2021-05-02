8b74fc859f4af9191818a4329dce6f7c6742247f
Yazan Qawasmeh <yazan929@users.noreply.github.com>
2021-05-02 09:53:48 +0800

    user can write reviews

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,3 +5,9 @@ const url = "http://localhost:6039/all";
 export const fetchPosts = () => axios.get(url);
 
 
+// fix this one according to the database
+// const url = "http://ec2-54-179-117-71.ap-southeast-1.compute.amazonaws.com:8080/api/shoes";
+const reviewsUrl = "http://localhost:6039/all_reviews";
+export const fetchReviews = () => axios.get(reviewsUrl);
+
+
